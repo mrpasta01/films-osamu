3c12249c960206012f0fba3bfcd4c37d4dc45431
Mrpasta01 <49362161+mrpasta01@users.noreply.github.com>
2024-05-19 18:37:01 +0700

    get_videos.php

--- osamu/api/get_videos.php
+++ osamu/api/get_videos.php
@@ -4,7 +4,7 @@
 	$c = 12;
 	$o = 0;
 	if(isset($_GET['count']))	$c = intval($_GET['count']);
-		if(isset($_GET['offset']))	$o = intval($_GET['offset']);
+	if(isset($_GET['offset']))	$o = intval($_GET['offset']);
 	
 	$sql = sprintf('SELECT `ID`, `Title`, `Opisanie`, `Rezishor`, `Year`, `POSTER` FROM `videos` LIMIT %d OFFSET %d', $c,$o);
 	$films = $conn->query($sql);
@@ -17,7 +17,7 @@
 	$YEAR = $row['Year'];
 	$POSTER =$row['POSTER'];
 	$Opisanie=$row['Opisanie'];
-	$result .= sprintf('{"Rezishor":"%s", "Title":"%s", "Year":"%d", "id":%d, "POSTER":"%s", "Opisanie":"%s"}, ', $Rezishor,$Title,$YEAR,$ID,$POSTER,$Opisanie);
+	$result .= sprintf('{"Rezishor":"%s", "Title":"%s", "Year":%d, "id":%d, "POSTER":"%s", "Opisanie":"%s"}, ', $Rezishor,$Title,$YEAR,$ID,$POSTER,$Opisanie);
 	}
 	$result = rtrim($result, ', ');
 	$result .= ']}';
