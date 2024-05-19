a4dcae9b491af147af8a460287fcf2edd36f6dc7
Mrpasta01 <49362161+mrpasta01@users.noreply.github.com>
2024-05-19 17:34:11 +0700

    script.js

--- osamu/js/script.js
+++ osamu/js/script.js
@@ -1,7 +1,6 @@
 function load_films(){
 		$.ajax({
-		//url: 'http://172.17.4.160/osamu/api/get_videos.php?count=11&offset=0',
-		url: 'http://217.71.129.139:4589/osamu/api/get_videos.php?count=11&offset=0',
+		url: 'http://217.71.129.139:4589/osamu/api/get_videos.php?count=12&offset=0',
 		method: 'get',  
 		dataType: 'json',
 		success: function(data){
@@ -9,8 +8,9 @@ function load_films(){
 			data['film'].forEach(function(film){
 				let div = $('<div class="film">')
 				div.append('<div class="foto"><img src="'+film['POSTER']+'"></div>')
-				div.append('<h6 class="ustal">'+film['Title']+', '+film['Year']+' год </h6>')
-				div.append('<p class="ustal">Режисёр: '+film['Rezishor']+'</p><p class="">'+film['Opisanie']+'</p>')
+				div.append('<p class="ustal"><b>'+film['Title']+'</b>, <i>'+film['Year']+'</i> год </p>')
+				div.append('<p class="ustal">Режисёр: <b>'+film['Rezishor']+'</b></p>')
+				div.append('<p class="opisanie">'+film['Opisanie']+'</p>')
 				$('#films').append(div)
 			})
 		}
