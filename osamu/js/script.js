d7b50e650880d439ae374205d1d270ebf924ab79
Mrpasta01 <49362161+mrpasta01@users.noreply.github.com>
2024-05-19 19:27:24 +0700

    script.js

--- osamu/js/script.js
+++ osamu/js/script.js
@@ -1,6 +1,22 @@
+let offset=0;
+
+function next(){
+	offset +=5;
+	load_films();
+}
+
+function prev(){
+	offset -=5;
+	load_films();
+}
+
 function load_films(){
+	if(offset==0)	$('#but_prev').attr('disabled', true)
+	if(offset>1)	$('#but_prev').attr('disabled', false)
+	if(offset==10) 	$('#but_next').attr('disabled', true)
+	if(offset<10) 	$('#but_next').attr('disabled', false) 
 		$.ajax({
-		url: 'http://217.71.129.139:4589/osamu/api/get_videos.php?count=12&offset=0',
+		url: 'http://217.71.129.139:4589/osamu/api/get_videos.php?count=5&offset='+offset,
 		method: 'get',  
 		dataType: 'json',
 		success: function(data){
@@ -12,6 +28,7 @@ function load_films(){
 				div.append('<p class="ustal">Режисёр: <b>'+film['Rezishor']+'</b></p>')
 				div.append('<p class="opisanie">'+film['Opisanie']+'</p>')
 				$('#films').append(div)
+				$(document).scrollTop(0)
 			})
 		}
 	});
